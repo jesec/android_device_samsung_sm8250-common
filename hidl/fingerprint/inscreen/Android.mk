f8c262e861b4122e43e425ccbf3437f0b169d0cd
Jesse Chan <jc@lineageos.org>
2020-06-29 19:37:58 +0800

    fingerprint: inscreen: use product/vendor_overlay
    Change-Id: Ia9e50ba35f7166ce5c6a64e3b07974b05abf896f

--- hidl/fingerprint/inscreen/Android.mk
+++ hidl/fingerprint/inscreen/Android.mk
@@ -34,12 +34,29 @@ LOCAL_SHARED_LIBRARIES := \
     libutils \
     vendor.lineage.biometrics.fingerprint.inscreen@1.0
 
-LOCAL_MODULE := vendor.lineage.biometrics.fingerprint.inscreen@1.0-service.samsung
-LOCAL_INIT_RC := vendor.lineage.biometrics.fingerprint.inscreen@1.0-service.samsung.rc
+LOCAL_REQUIRED_MODULES := \
+    vendor.lineage.biometrics.fingerprint.inscreen@1.0-service.samsung.rc
+
+LOCAL_MODULE := vendor.lineage.biometrics.fingerprint.inscreen@1.0-service.samsung-sm8250
+LOCAL_MODULE_STEM := vendor.lineage.biometrics.fingerprint.inscreen@1.0-service.samsung
+LOCAL_MODULE_PATH := $(TARGET_OUT_PRODUCT)/vendor_overlay/$(PLATFORM_VNDK_VERSION)/bin
 LOCAL_MODULE_RELATIVE_PATH := hw
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_OWNER := samsung
-LOCAL_VINTF_FRAGMENTS := vendor.lineage.biometrics.fingerprint.inscreen@1.0-service.samsung.xml
-LOCAL_VENDOR_MODULE := true
+
+LOCAL_POST_INSTALL_CMD := mkdir -p $(TARGET_OUT_PRODUCT)/vendor_overlay/$(PLATFORM_VNDK_VERSION)/lib64
+LOCAL_POST_INSTALL_CMD += ; touch $(TARGET_OUT_PRODUCT)/vendor_overlay/$(PLATFORM_VNDK_VERSION)/lib64/vendor.lineage.biometrics.fingerprint.inscreen@1.0.so
 
 include $(BUILD_EXECUTABLE)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := vendor.lineage.biometrics.fingerprint.inscreen@1.0-service.samsung.rc
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+
+LOCAL_MODULE_PATH := $(TARGET_OUT_PRODUCT)/vendor_overlay/$(PLATFORM_VNDK_VERSION)/etc/init
+
+LOCAL_SRC_FILES := $(LOCAL_MODULE)
+
+include $(BUILD_PREBUILT)
